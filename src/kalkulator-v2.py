9ae0f22ee46667052ca209c5344a296f725b5127
Knast <cknast@wfosgw.poznan.pl>
2025-05-05 13:24:19 +0200

    Kalkulator v-2

---
+++ src/kalkulator-v2.py
@@ -0,0 +1,7 @@
+a = float (input ("Podaj pierwszą liczbę "))
+b = float (input ("Podaj drugą liczbę "))
+print("Suma:", a+b)
+print("Różnica:", a-b)
+print("Iloczyn:", a*b)
+print("Iloraz:", a/b)
+print("Reszta z dzielenia:", a%b)
