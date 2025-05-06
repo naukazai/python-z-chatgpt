f2f0532107be86e3314a12fce18b9e6298e936ca
Knast <cknast@wfosgw.poznan.pl>
2025-05-06 15:08:21 +0200

    Pętle for while

--- src/funkcje-podatek.py
+++ src/funkcje-podatek.py
@@ -1,4 +1,13 @@
-def greet(name):
-    return f"Cześć, {name}!"
+#Zrób funkcję oblicz_vat(cena, stawka) i zwróć cenę brutto.
 
-print(greet("Kasia"))
+def oblicz_vat (cena, stawka):
+    return round(cena * (1 + stawka), 2)
+
+try:
+    cena_netto = float (input("Podaj cenę netto (np. 1245)"))
+    stawka_vat = float (input("Podaj stawkę wat w formie dziesiątnej (np. 0.23 dla 23%)"))
+    cena_brutto = oblicz_vat(cena_netto, stawka_vat)
+    print (f"Cena brutto: {cena_brutto} zł")
+except ValueError:
+    print("Błąd: Wprowadź poprawne liczby!")
+    
